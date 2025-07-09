395975f53fdde11ca1f71196b8f5c5bee9f00c41
Snayderth Campo <snayderthdev@gmail.com>
2025-07-09 17:52:35 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+self.addEventListener("install", (event) => {
+  console.log("🧙‍♂️ Service Worker instalado");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("✨ Service Worker activado");
+});
+
+self.addEventListener("fetch", (event) => {
+  // Deja pasar todas las peticiones por ahora
+});
